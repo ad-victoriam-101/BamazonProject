b799ab1e7ffb6d7d24734592b143962de0b62493
ad-victoriam-101 <keeper24@gmail.com>
2018-09-02 13:21:59 -0700

    Initial db creation and readme.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# BamazonProject
+# Bamazon Project
 A rip off of Amazon plz no sue Jeff Bezos
+# Pseudo Code
+1. Create a DB for backend.
+    * make sure the db has the correct col and id
+2. Make products table in back end.
+    * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+3. useing inquirer prompt user inputs for buying an item off of bamazon.
+    * use pretty console outputs.
+4. make sure item user requested is in stock.
+    * simple if statement
+5. if item is instock deduct number they asked for and gir rid of that many items in db. 
+    * remember that its -= the item
+6. add cost of sold items to profit margin. 
---
+++ bamazon_DB.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dog Kennel", "HomeProducts",50.00,10),("Dog food","HomeProducts", 22.30,100),("Dog Bowl","HomeProducts",10.20,100),("White Wine","Grocery",5.50,200),("Heaphones","Electronics",200.50,50),("iPhoneX","Electronics",1000.10,10),("Table lamp","HomeProducts", 20.00,50),("Laptop bag","Electronics",30.00,40),("Coffee Beans(whole)","Grocery",20.20,100),("Laptop Charger","Electronics",50.00,30);
+SELECT * FROM products;
