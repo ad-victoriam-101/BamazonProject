760ffa1b3e2dd9542d3aff06f7597a23fb33b635
ad-victoriam-101 <keeper24@gmail.com>
2018-09-02 13:39:40 -0700

    started tables calls in js.

--- bamazon.js
+++ bamazon.js
@@ -1,20 +1,33 @@
 // Bamazon.com
 var mysql = require("mysql");
+const inquirer = require("inquirer");
+const Table = require('cli-table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "bamazon_DB"
-});connection.connect(function(err) {
+    // Your password
+    password: "",
+    database: "bamazon_DB"
+});
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     bamazonStart();
-  });
+});
+
+function bamazonStart(); {
+    console.log("Welcome to Bamazon!")
+    connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.log(res);
+            connection.end();
+        });
+}
